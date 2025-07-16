17684e5aa536df66ba7ff2380192ac7922af0d5e
AllanBit6 <allanvasquez1312@gmail.com>
2025-07-16 17:07:26 -0600

    Inicio del proyecto

---
+++ app/main.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI()
+
+app.middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True, # Allows cookies to be sent
+    allow_methods=["*"],    # Allows all HTTP methods
+    allow_headers=["*"],    # Allows all headers
+)
+
+connectedClients = {}
+
+@app.websocket("/ws/{client_id}")
+async def websocket_endpoint(websocket: WebSocket, client_id: str):
+    await websocket.accept()
+    connectedClients[client_id] = websocket
+
+    try:
+        while True:
+            data = await websocket.receive_text()
+            print(f"[{client_id}] dice {data}")
+    except WebSocketDisconnect:
+        print(f"{client_id} desconectado")
+        del connectedClients[client_id]
+
+@app.post("/send/{client_id}")
+async def send_command(client_id: str, command: str):
+    if client_id in connectedClients:
+        await connectedClients[client_id].send_text(command)
+        return {"message": f"Comando '{command}' enviado a {client_id}"}
+    else:
+        return {"error": "Cliente no conectado"}
